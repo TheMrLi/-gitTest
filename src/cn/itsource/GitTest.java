293d9e2af16db68da3b42dc862ffd26921e44a41
user01 <user01@123456>
2019-05-06 18:59:48 +0800

    添加

---
+++ src/cn/itsource/GitTest.java
@@ -0,0 +1,11 @@
+package cn.itsource;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这是git测试");
+
+        System.out.println("asydisahdsah");
+
+		System.out.println("11111111111111");
+    }
+}
